62ffc7169ffab0dd51f436d781a05369ad48e507
Simon Martinelli <simon@72.services>
2022-10-19 18:28:27 +0200

    Renaming

--- src/main/java/ch/martinelli/vaadin/workshop/Application.java
+++ src/main/java/ch/martinelli/vaadin/workshop/Application.java
@@ -10,14 +10,13 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 /**
  * The entry point of the Spring Boot application.
- *
+ * <p>
  * Use the @PWA annotation make the application installable on phones, tablets
  * and some desktop browsers.
- *
  */
 @SpringBootApplication
-@Theme(value = "wstage22-vaadin")
-@PWA(name = "wstage22-vaadin", shortName = "wstage22-vaadin", offlineResources = {})
+@Theme(value = "vaadin-workshop")
+@PWA(name = "Vaadin Workshop", shortName = "vaadin-workshop", offlineResources = {})
 @NpmPackage(value = "line-awesome", version = "1.3.0")
 @NpmPackage(value = "@vaadin-component-factory/vcf-nav", version = "1.0.6")
 public class Application extends SpringBootServletInitializer implements AppShellConfigurator {
