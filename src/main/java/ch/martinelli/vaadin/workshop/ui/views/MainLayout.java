71633512d41df3ce855624d898c4576e098190d2
Simon Martinelli <simon@72.services>
2022-10-20 15:20:58 +0200

    Fixed navigation

--- src/main/java/ch/martinelli/vaadin/workshop/ui/views/MainLayout.java
+++ src/main/java/ch/martinelli/vaadin/workshop/ui/views/MainLayout.java
@@ -5,6 +5,8 @@ import ch.martinelli.vaadin.workshop.ui.components.appnav.AppNav;
 import ch.martinelli.vaadin.workshop.ui.components.appnav.AppNavItem;
 import ch.martinelli.vaadin.workshop.ui.views.about.AboutView;
 import ch.martinelli.vaadin.workshop.ui.views.helloworld.HelloWorldView;
+import ch.martinelli.vaadin.workshop.ui.views.participant.ParticipantView;
+import ch.martinelli.vaadin.workshop.ui.views.workshop.WorkshopView;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.applayout.DrawerToggle;
@@ -58,6 +60,9 @@ public class MainLayout extends AppLayout {
         AppNav nav = new AppNav();
         nav.addClassNames("app-nav");
 
+
+        nav.addItem(new AppNavItem("Workshops", WorkshopView.class, "la la-chalkboard-teacher"));
+        nav.addItem(new AppNavItem("Participants", ParticipantView.class, "la la-restroom"));
         nav.addItem(new AppNavItem("Hello World", HelloWorldView.class, "la la-globe"));
         nav.addItem(new AppNavItem("About", AboutView.class, "la la-file"));
 
