4e2f8b14d32851724a5ee1c0873159d0da73ff3a
Simon Martinelli <simon@72.services>
2024-02-01 13:57:39 +0100

    Fixed test

--- src/test/java/ch/martinelli/vaadin/workshop/ui/views/helloworld/HelloWorldViewIT.java
+++ src/test/java/ch/martinelli/vaadin/workshop/ui/views/helloworld/HelloWorldViewIT.java
@@ -3,19 +3,19 @@ package ch.martinelli.vaadin.workshop.ui.views.helloworld;
 import com.vaadin.flow.component.button.testbench.ButtonElement;
 import com.vaadin.flow.component.notification.testbench.NotificationElement;
 import com.vaadin.flow.component.textfield.testbench.TextFieldElement;
+import com.vaadin.testbench.BrowserTest;
 import com.vaadin.testbench.BrowserTestBase;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 
 public class HelloWorldViewIT extends BrowserTestBase {
 
     @BeforeEach
     public void setup() {
-        getDriver().get("http://localhost:8080/");
+        getDriver().get("http://localhost:8080/hello");
     }
 
-    @Test
+    @BrowserTest
     public void say_hello() {
         $(TextFieldElement.class).first().setValue("Peter Muster");
         $(ButtonElement.class).first().click();
