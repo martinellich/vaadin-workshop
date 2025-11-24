2bd0e9689c6322a918053e9cf00c06ed9248e1be
Simon Martinelli <simon@martinelli.ch>
2025-11-24 09:08:29 +0100

    Integrate Playwright for UI testing: add PlaywrightIT base class, HelloWorldViewPlaywrightIT, and WorkshopViewIT; update dependencies to include Playwright and Mopo. Rename HelloWorldViewIT to HelloWorldViewTestbenchIT.

---
+++ src/test/java/ch/martinelli/vaadin/workshop/ui/views/PlaywrightIT.java
@@ -0,0 +1,53 @@
+package ch.martinelli.vaadin.workshop.ui.views;
+
+import com.microsoft.playwright.*;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public abstract class PlaywrightIT {
+
+	@LocalServerPort
+	protected Integer localServerPort;
+
+	private static Playwright playwright;
+
+	private static Browser browser;
+
+	protected Page page;
+
+	private BrowserContext browserContext;
+
+	@BeforeAll
+	static void setUpClass() {
+		playwright = Playwright.create();
+		BrowserType browserType = playwright.chromium();
+		BrowserType.LaunchOptions launchOptions = new BrowserType.LaunchOptions();
+		// set to false if you want to see the browser during development
+		launchOptions.headless = true;
+		browser = browserType.launch(launchOptions);
+	}
+
+	@AfterAll
+	static void tearDownClass() {
+		browser.close();
+		playwright.close();
+	}
+
+	@BeforeEach
+	void setUp() {
+		browserContext = browser.newContext();
+		page = browserContext.newPage();
+	}
+
+	@AfterEach
+	void tearDown() {
+		page.close();
+		browserContext.close();
+	}
+
+}
