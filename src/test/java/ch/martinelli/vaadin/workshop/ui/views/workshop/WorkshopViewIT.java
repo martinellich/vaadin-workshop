5167bbaef62fec98b1ae472004bee20e8d19400b
Simon Martinelli <simon@martinelli.ch>
2025-11-24 09:40:25 +0100

    Refactor WorkshopViewIT: use PlaywrightIT base class and replace TestBench methods with Playwright alternatives

--- src/test/java/ch/martinelli/vaadin/workshop/ui/views/workshop/WorkshopViewIT.java
+++ src/test/java/ch/martinelli/vaadin/workshop/ui/views/workshop/WorkshopViewIT.java
@@ -1,28 +1,14 @@
 package ch.martinelli.vaadin.workshop.ui.views.workshop;
 
-import com.vaadin.flow.component.button.testbench.ButtonElement;
-import com.vaadin.flow.component.notification.testbench.NotificationElement;
-import com.vaadin.flow.component.textfield.testbench.TextFieldElement;
-import com.vaadin.testbench.BrowserTest;
-import com.vaadin.testbench.BrowserTestBase;
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
+import ch.martinelli.vaadin.workshop.ui.views.PlaywrightIT;
+import org.junit.jupiter.api.Test;
 
-public class WorkshopViewIT extends BrowserTestBase {
+class WorkshopViewIT extends PlaywrightIT {
 
-    @BeforeEach
-    public void setup() {
-        getDriver().get("http://localhost:8080/hello");
-    }
+    @Test
+    void filter() {
+        page.navigate("http://localhost:%d".formatted(localServerPort));
 
-    @BrowserTest
-    public void say_hello() {
-        $(TextFieldElement.class).first().setValue("Peter Muster");
-        $(ButtonElement.class).first().click();
-
-        NotificationElement notificationElement = $(NotificationElement.class).first();
-
-        Assertions.assertThat(notificationElement.getText()).isEqualTo("Hello Peter Muster");
     }
 
 }
